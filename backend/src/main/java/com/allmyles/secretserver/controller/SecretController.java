636b08beadc7c2bd25df9b547c29f364b6c0bd6c
Molnár Levente <112870372+molnarlevi19@users.noreply.github.com>
2024-01-24 14:57:09 +0100

    use crossorigin annotation on controller class

--- backend/src/main/java/com/allmyles/secretserver/controller/SecretController.java
+++ backend/src/main/java/com/allmyles/secretserver/controller/SecretController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/secrets")
+@CrossOrigin(origins = "http://localhost:5173")
 public class SecretController {
 
     private final SecretRepository secretRepository;
