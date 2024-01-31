252f3972b5010afa3a9fea3d72d5947771e1bfe9
Molnár Levente <112870372+molnarlevi19@users.noreply.github.com>
2024-01-31 12:43:32 +0100

    refactor vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,18 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode}) => {
+const env = loadEnv(mode, process.cwd(), '')
+  return{
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: env.VITE_BACKEND_URL,
         changeOrigin: true,
         secure: false,
       },
     },
   },
-})
+}})
